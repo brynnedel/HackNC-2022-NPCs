4290865ad21fee58ce07bfaa55f1101bbd6fd876
Glitch (occipital-flossy-stilton) <none>
2022-11-06 03:17:33 +0000

    🎀💐 Checkpoint ./src/styles/styles.css:50688511/130 ./src/components/form.jsx:50688509/252 ./index.html:50688511/237 ./src/pages/testing.jsx:50688511/18

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -7,7 +7,7 @@ import { WebrtcProvider } from 'y-webrtc';
 const ydoc = new Y.Doc()
 // clients connected to the same room-name share document updates
 const provider = new WebrtcProvider('your-room-name', ydoc, { password: 'optional-room-password' })
-const yarray = ydoc.get('array', Y.Array)
+const yarray = ydoc.getArray('array')
 
 function TableData() {
   const [artistData, setArtistData] = useState(jsonData);
@@ -25,7 +25,7 @@ function TableData() {
   const addRows = (data) => {
     const totalArtists = artistData.length;
     const updatedArtistData = [...artistData];
-    yarray.push(updatedArtistData.push(data));
+    yarray.push(data);
     setArtistData(updatedArtistData);
   };
   
