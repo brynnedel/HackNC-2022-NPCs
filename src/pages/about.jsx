8855a81aae3395226909d3b585df4707ef8d87b2
Glitch (occipital-flossy-stilton) <none>
2022-11-06 00:02:37 +0000

    🌲🍇 Checkpoint ./src/styles/styles.css:50688511/162 ./src/pages/about.jsx:50688511/1214 ./src/components/form.jsx:50688509/1345

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -7,8 +7,25 @@ export default function About() {
   return (
     <div className="page">
       
-      <h1 className="title">
-        About the website
+      <h1 class="title">
+         <span class="change-color">A</span>
+        <span class="change-color">b</span>
+        <span class="change-color">o</span>
+        <span class="change-color">u</span>
+        <span class="change-color">t</span>
+        <span class="hide">l</span>
+         <span class="change-color">t</span>
+        <span class="change-color">h</span>
+        <span class="change-color">e</span>
+         <span class="hide">l</span>
+        <span class="change-color">w</span>
+        <span class="change-color">e</span>
+        <span class="change-color">b</span>
+         <span class="change-color">s</span>
+        <span class="change-color">i</span>
+        <span class="change-color">t</span>
+        <span class="change-color">e</span>
+       
       </h1>
       <p >
         Welcome to the UNC-CH Artist finder. where you can instantly find artists in the
