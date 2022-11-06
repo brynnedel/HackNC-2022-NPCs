59229e8fbf3b439b3e52b98177271c05f3d7a4f6
Glitch (occipital-flossy-stilton) <none>
2022-11-06 04:33:34 +0000

    🐮🌘 Checkpoint ./src/styles/styles.css:50688511/107 ./src/components/form.jsx:50688509/1098 ./src/seo.json:50693772/110

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -1,48 +1,41 @@
-import React, {useState, useEffect, useRef} from 'react';
+import React, {useState, useEffect} from 'react';
 import ArtistsForm from '../components/table';
 import jsonData from './data.json';
 import * as Y from 'yjs';
 import { WebrtcProvider } from 'y-webrtc';
 
 const ydoc = new Y.Doc()
-// // clients connected to the same room-name share document updates
+// clients connected to the same room-name share document updates
 const provider = new WebrtcProvider('your-room-name', ydoc, { password: 'optional-room-password' })
-// const yarray = ydoc.getArray('array')
+const yarray = ydoc.getArray('array')
 
 // yarray.observeDeep(() => {
 //   console.log('yarray updated: ', yarray.toJSON())
 // })
 
 function TableData() {
-  // clients connected to the same room-name share document updates
-  const yarray = useRef(ydoc.getArray('array'))
   const [artistData, setArtistData] = useState([{"name": "", "location": "", "social": ""}]);
-  const [arrayValue, setArrayValue] = useState(yarray.current.toArray());
+  const [arrayValue, setArrayValue] = useState(yarray.toArray());
   
-  yarray.current.observeDeep(() => {
-  console.log('yarray updated: ', yarray.current.toJSON())
-})
-
-  // const tableRows = yarray.map((info) => {
-  //   return (
-  //     <tr>
-  //       <td>{info.name}</td>
-  //       <td>{info.location}</td>
-  //       <td>{info.social}</td>
-  //     </tr>
-  //   );
-  // });
+  const tableRows = yarray.map((info) => {
+    return (
+      <tr>
+        <td>{info.name}</td>
+        <td>{info.location}</td>
+        <td>{info.social}</td>
+      </tr>
+    );
+  });
   useEffect(()=> {
-    setArrayValue(yarray.current.toArray());
+    setArrayValue(yarray.toArray());
   }, []);
   const addRows = (data) => {
     const totalArtists = artistData.length;
     const updatedArtistData = [...artistData];
     console.log(updatedArtistData);
     console.log(data);
-    yarray.current.push([data]);
-    setArrayValue(yarray.current.toArray());
-    console.log(arrayValue);
+    yarray.push([data]);
+    setArrayValue(yarray.toArray());
   };
   
   return (
@@ -55,7 +48,7 @@ function TableData() {
             <th>Social Media</th>
           </tr>
         </thead>
-        <tbody>{yarray.cur.map((info) => {
+        <tbody>{arrayValue.map((info) => {
             <tr>
               <td>{info.name}</td>
               <td>{info.location}</td>
