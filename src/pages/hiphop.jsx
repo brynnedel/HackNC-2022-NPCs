cef76c1fd946590be124cee9ab1ac177966edc66
Glitch (occipital-flossy-stilton) <none>
2022-11-06 14:19:29 +0000

    🌘🚌 Checkpoint ./src/styles/styles.css:50688511/1631 ./src/pages/home.jsx:50693772/30788 ./src/pages/hiphop.jsx:50688511/93 ./src/pages/indie.jsx:50688511/90 ./src/pages/slideshowtest.jsx:50693772/1202

--- src/pages/hiphop.jsx
+++ src/pages/hiphop.jsx
@@ -4,6 +4,9 @@ import TableData from '../components/form';
 function Rnb() {
   return (
     <div classname="page">
+        <div id="banner-hiphop">
+        </div>
+       <div class="divider"></div>
       <h1>Hip Hop </h1>
       <TableData genre="Hiphop"/>
     </div>
--- src/pages/indie.jsx
+++ src/pages/indie.jsx
@@ -4,6 +4,9 @@ import TableData from '../components/form';
 function Indie() {
   return (
     <div classname="page">
+        <div id="banner-indie">
+        </div>
+       <div class="divider"></div>
       <h1>Indie</h1>
       <TableData genre="Indie"/>
     </div>
